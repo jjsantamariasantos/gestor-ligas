6744cca2a8ada9dba3be8838ef78f5b93c180d40
jjsantamariasantos <j.j.ssantos@udc.es>
2025-03-28 11:24:16 +0100

    backedn/db.js modificada y backend.env y routes/authroutes.js creados

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,19 +1,15 @@
 // backend/config/db.js
 const mysql = require('mysql2');
+require.dotenv().config(); // Cargar variables de entorno desde .env
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0 
+}).promise(); // Promisify the pool to use async/await
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',         // Usuario root o el que hayas creado
-  password: 'admin',
-  database: 'gestor_ligas',
-});
-
-connection.connect((err) => {
-  if (err) {
-    console.error('Error de conexión: ' + err.stack);
-    return;
-  }
-  console.log('Conectado a la base de datos MariaDB');
-});
-
-module.exports = connection;
+module.exports = pool;
+// backend/config/db.js
